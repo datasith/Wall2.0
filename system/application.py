20f3aebef3740b1f01f04d3b01837c6040fc8d32
Cisco (MakerBro) Zabala <makerbro@acrobotic.com>
2015-11-20 13:26:14 +0000

    updating

--- system/application.py
+++ system/application.py
@@ -5,9 +5,16 @@ print "Initializing"
 p=pysixad()     #Create a PS3 object
 m=motion()
 print "Done"
+
 up_counter=1
 down_counter=1
+left_counter=1
+right_counter=1
+cross_counter = 1
+circle_counter = 1
+
 sensitivity=7
+
 while True:
     p.update()          #Read the ps3 values
     if p.up:            #If UP is pressed move forward
@@ -15,19 +22,30 @@ while True:
     elif p.down:        #If DOWN is pressed go back
         down_counter+=1
     elif p.left:        #If LEFT is pressed turn left
-        print "left"
+        left_counter+=1
     elif p.right:       #If RIGHT is pressed move right
-        print "right"
-    elif p.cross:       #If CROSS is pressed stop
+        right_counter+=1
+    elif p.cross:       #If CROSS is pressed record position
         cross_counter+=1
+    elif p.circle:      #If CIRCLE is pressed playback positions
+        circle_counter+=1
     else:
         up_counter = 1
         down_counter = 1
+        left_counter = 1
+        right_counter = 1
         cross_counter = 1
+        circle_counter = 1
     if not (up_counter % sensitivity):
         m.fwd()
     if not (down_counter % sensitivity):
         m.bwd()
+    if not (left_counter % sensitivity):
+        m.left()
+    if not (right_counter % sensitivity):
+        m.right()
     if not (cross_counter % sensitivity):
-        m.get_current_position()
+        m.record_current_position()
+    if not (circle_counter % sensitivity):
+        m.playback_saved_positions()
     time.sleep(.01)
--- system/motion.py
+++ system/motion.py
@@ -6,18 +6,28 @@ Simple g-code streaming script for grbl
 import serial
 import time
 import json
-from multiprocessing import Process
- 
+from multiprocessing import Process, Queue
 class motion():
     def __init__(self):
-        self.serial_reader = Process(target=self.get_response,args=())
         # Open grbl serial port
         #self.s = serial.Serial("/dev/ttyUSB0",baudrate=115200,xonxoff=True,timeout=1)
-        self.s = serial.Serial("/dev/ttyUSB0",baudrate=115200,timeout=0.1,rtscts=True,xonxoff=False)
+        self.s = serial.Serial("/dev/ttyUSB0",
+                               baudrate=115200,
+                               timeout=0.1,
+                               rtscts=True,
+                               xonxoff=False)
 
         self.rsp=''
         self.posx=0.0
         self.posy=0.0
+
+        self.positions_file = 'positions.csv'
+        with open(self.positions_file,'w') as f:
+            f.write('posx,posy\n')
+
+        self.pos_queue = Queue()
+        self.serial_proc = Process(target=self.get_response,
+                                   args=(self.pos_queue,))
 
         # Wake up grbl
         self.s.write("\r\n\r\n")
@@ -27,39 +37,79 @@ class motion():
         self.send("g92 x0 y0")
         # feedrate speed
         self.send("f400")
-        #self.send("g91")
+        # relative mode 
+        self.send("g91")
 
-        self.serial_reader.start()
+        self.serial_proc.start()
  
     def send(self, cmd): 
         print 'Sending: ' + cmd
         self.s.write(cmd + '\n') # Send g-code block to grbl
 
-    def get_response(self):
+    def get_response(self, q):
         while(1):
             tmp = self.s.readline()
             tmp = tmp.strip()
             if tmp is not '':
                 tmp = json.loads(tmp)
-                self.rsp=tmp
+                print tmp
+                if 'r' in tmp.keys():
+                    if 'sr' in tmp['r'].keys():
+                        tmp = tmp['r']
                 if 'sr' in tmp.keys():
                     if 'posx' in tmp['sr'].keys():
                         self.posx=tmp['sr']['posx']
-                        print 'pos: '+str(self.posx)
+                    if 'posy' in tmp['sr'].keys():
+                        self.posy=tmp['sr']['posy']
+                    q.put((self.posx, self.posy))
+                    print 'pos1: '+str((self.posx, self.posy))
             else:
                 time.sleep(.2)
 
-    def get_current_position(self):
-        self.send("sr")
-        # Check if serial_reader is running?
-        print "get_current_position()"
-        print 'pos: '+str(self.posx)
+    def record_current_position(self):
+        self.send('{"sr":null}')
+        # TODO: Check if serial_proc is running?
+        self.update_current_position()
+        with open(self.positions_file,'a') as f:
+            f.write(str(self.posx)+','+str(self.posy)+'\n')
+
+    def update_current_position(self):
+        while not self.pos_queue.empty():
+            self.posx, self.posy = self.pos_queue.get()
+
+    def playback_saved_positions(self):
+        import csv,time
+        self.update_current_position()
+        with open(self.positions_file) as f:
+            lines = csv.DictReader(f)
+            for l in lines:
+                print 'x_dst: '+l['posx']+' - '+str(self.posx)
+                print 'y_dst: '+l['posy']+' - '+str(self.posy)
+                x_dst = float(l['posx'])-self.posx
+                y_dst = float(l['posy'])-self.posy
+                x = ' x'+str(int(x_dst))
+                y = ' y'+str(int(y_dst))
+                print(x,y)
+                self.send('g1'+x+y)
+                while(1):
+                    self.update_current_position()
+                    if (self.posx != float(l['posx'])) or \
+                       (self.posy != float(l['posy'])):
+                       time.sleep(1)
+                    else:
+                        break
 
     def fwd(self):
-        self.send("g1 x1")
+        self.send("g1 x1 y0")
 
     def bwd(self):
-        self.send("g1 x-1")
+        self.send("g1 x-1 y0")
+
+    def right(self):
+        self.send("g1 x0 y1")
+
+    def left(self):
+        self.send("g1 x0 y-1")
 
     def disconnect(self):
         # Close file and serial port
